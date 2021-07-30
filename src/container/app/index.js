e332a300dff0f5821d04e8b622b5dd86296f00cc
Cres333 <ca.mikaduki@gmail.com>
2021-07-31 08:24:35 +0900

    feat: customized page

---
+++ src/container/app/index.js
@@ -0,0 +1,15 @@
+import logo from '../../assets/logo.svg';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>開発中</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
