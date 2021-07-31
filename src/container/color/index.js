62bc637d5f1499825a1a9df4c5cc1287ef5d63c2
Cres333 <ca.mikaduki@gmail.com>
2021-07-31 11:04:39 +0900

    feat: reverse color

--- src/container/color/index.js
+++ src/container/color/index.js
@@ -1,18 +1,33 @@
 import React, { useState, useCallback } from 'react';
 import { SketchPicker } from 'react-color';
+import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    margin: 16,
+  },
+});
 
 const ColorTool = () => {
+  const style = useStyles();
   const [color, SetColor] = useState('#FFFFFF');
   const intColor = parseInt(color.replace('#', ''), 16);
 
-  const updateColor = useCallback((color, event) => {
+  const updateColor = useCallback((color) => {
     SetColor(color.hex);
+  }, []);
+
+  const convertColor = useCallback((event) => {
+    const num = parseInt(event.target.value, 10);
+    const colorNum = 0 <= num && num <= 0xffffff ? num : 0;
+    SetColor('#' + colorNum.toString(16).padStart(6, '0'));
   }, []);
 
   return (
     <>
       <SketchPicker color={color} onChange={updateColor} />
-      <p>Biome Color: {intColor}</p>
+      <TextField className={style.root} label="Biome Color" variant="filled" value={intColor} onChange={convertColor} />
     </>
   )
 };
