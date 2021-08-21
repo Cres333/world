f6ae10010450cf087724cb28aeb2007a0e6f4363
Cres333 <ca.mikaduki@gmail.com>
2021-08-21 09:07:48 +0900

    feat: add version and preset for rules

--- src/container/tools/rule/index.js
+++ src/container/tools/rule/index.js
@@ -1,26 +1,29 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Button, TextField } from '@material-ui/core';
+import { FormControl, Select, MenuItem, Button, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import { fetchCsv } from '../../../utils/csv';
 import { WorldContext } from '../../root';
 
-const useStyles = makeStyles({
-  root: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+const useStyles = makeStyles((theme) => ({
+  top: {
+    flexDirection: 'row',
+  },
+  form: {
+    margin: theme.spacing(1),
+    minWidth: 120,
   },
   field: {
     margin: 16,
   },
-});
+}));
 
-const convertMcFunctionText = (data, version) => {
+const convertMcFunctionText = (data, version, preset) => {
   const textData = data
     .filter(item => parseFloat(item.version) <= version)
     .map(item => {
+      console.log(`${JSON.stringify(item)}`);
       const title = `## ${item.desc}`;
       const note = (() => {
         if (item.note2) return `# ${item.note1}\n # ${item.note2}`;
@@ -28,7 +31,7 @@ const convertMcFunctionText = (data, version) => {
         return null;
       })();
       const def = `# 初期値: ${item.default}`;
-      const code = `gamerule ${item.key} ${item.default}`;
+      const code = `gamerule ${item.key} ${item[preset]}`
 
       if (note) {
         return `${title}\n${note}\n${def}\n${code}`;
@@ -42,6 +45,7 @@ const convertMcFunctionText = (data, version) => {
 const RuleTool = () => {
   const style = useStyles();
   const [version, SetVersion] = useState(1.17);
+  const [preset, SetPreset] = useState('preset-world');
   const [value, SetValue] = useState('');
   const { dispatch } = useContext(WorldContext);
 
@@ -52,16 +56,36 @@ const RuleTool = () => {
   useEffect(() => {
     const fetchData = (async () => {
       const data = await fetchCsv('/world/assets/rules.csv');
-      SetValue(convertMcFunctionText(data, version));
+      SetValue(convertMcFunctionText(data, version, preset));
     });
     fetchData();
-  }, [version]);
+  }, [version, preset]);
 
   return (
     <>
-      <CopyToClipboard text={value}>
-        <Button variant="contained">コピー</Button>
-      </CopyToClipboard>
+      <div className={style.top}>
+        <FormControl className={style.form}>
+          <Select value={version} onChange={e => SetVersion(e.target.value)}>
+            <MenuItem value={1.12}>ver 1.12</MenuItem>
+            <MenuItem value={1.13}>ver 1.13</MenuItem>
+            <MenuItem value={1.14}>ver 1.14</MenuItem>
+            <MenuItem value={1.15}>ver 1.15</MenuItem>
+            <MenuItem value={1.16}>ver 1.16</MenuItem>
+            <MenuItem value={1.17}>ver 1.17</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={style.form}>
+          <Select value={preset} onChange={e => SetPreset(e.target.value)}>
+            <MenuItem value="default">デフォルト</MenuItem>
+            <MenuItem value="preset-world">マップ作成用</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={style.form}>
+          <CopyToClipboard text={value}>
+            <Button variant="contained">コピー</Button>
+          </CopyToClipboard>
+        </FormControl>
+      </div>
       <TextField
         className={style.field}
         label="rules.mcfunction"
@@ -72,7 +96,7 @@ const RuleTool = () => {
         fullWidth
       />
     </>
-  )
+  );
 };
 
 export default RuleTool;
